6e80b1504f4aa7793f7fb0f44b8024d0e8dbe55f
Peeyush Yadav <peeyushy95@gmail.com>
2017-10-17 21:35:54 +0530

    Request Param Handling

--- HttpServer/README.md
+++ HttpServer/README.md
@@ -1,6 +1,15 @@
 # Http Server
+---------
 
-### How to Run
+## How to Run
+---------
 
 python server.py [port] <br />
 Note : port is optional. if not provide server will run at 8081 port.
+
+## Configurations
+---------
+
+set `InputParamConsidered` Flag to `True`, if request is required to be handled based on request params. <br />
+if `InputParamConsidered` Flag is `True`, by default only `http://localhost:8081/p1` request will be served.
+modify `pathsAllowed` and `send_param_based_resp` func accordingly
--- HttpServer/server.py
+++ HttpServer/server.py
@@ -4,27 +4,50 @@ import os,json
 from sys import argv
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+JsonContent = 'application/json'
+HtmlContent = 'text/html'
+
+InputParamConsidered = False
+pathsAllowed = ['/p1']
+
 class HttpHandler(BaseHTTPRequestHandler):
 
-    def set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
+    def set_headers(self,contentType, Resp = 200):
+        self.send_response(Resp)
+        self.send_header('Content-type',contentType)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
     def do_GET(self):
-        self.set_headers()
         file = os.path.join(os.path.dirname(__file__), 'data.json')
         f = open(file, 'rb')
         file_data = f.read()
         f.close()
-        self.wfile.write(file_data)
+        if InputParamConsidered == False :
+            self.set_headers(JsonContent)
+            self.send_rqst_resp(file_data)
+        else:
+            self.send_param_based_resp()
 
     def do_POST(self):
-        self.set_headers()
         posData = self.rfile.read(int(self.headers['Content-Length']))
         # Return Post data back
-        self.wfile.write(posData)
+        if InputParamConsidered == False :
+            self.set_headers(JsonContent)
+            self.send_rqst_resp(posData)
+        else:
+            self.send_param_based_resp()
+
+    def send_rqst_resp(self,data):
+        self.wfile.write(data)
+
+    def send_param_based_resp(self):
+        if self.path  ==  pathsAllowed[0]:
+            self.set_headers(JsonContent)
+            self.send_rqst_resp(json.dumps({"resp" : "p1 was parameter"}).encode())
+        else:
+            self.set_headers(HtmlContent,404)
+            self.send_error(404)
 
 if __name__ == "__main__":
     port = int(argv[1]) if len(argv) == 2 else 8081
